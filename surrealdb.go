93541fae171e2a3c51ab7b427ffddaa9855e7a17
Matthew Davis <matthew@matthewdavis.io>
2024-11-16 20:19:40 -0600

    bump

--- surrealdb.go
+++ surrealdb.go
@@ -1,8 +1,6 @@
 package schema
 
 import (
-	"fmt"
-
 	"github.com/surrealdb/surrealdb.go"
 )
 
@@ -22,62 +20,65 @@ func ConnectSurrealDB(url string, user string, pass string, name string, namespa
 		return nil, err
 	}
 
-	if _, err = db.Signin(map[string]interface{}{
-		"user": user,
-		"pass": pass,
-	}); err != nil {
+	if err = db.Use(name, namespace); err != nil {
 		return nil, err
 	}
 
-	if _, err = db.Use(name, namespace); err != nil {
-		return nil, err
+	// Sign in to authentication `db`
+	authData := &surrealdb.Auth{
+		Username: "root", // use your setup username
+		Password: "root", // use your setup password
+	}
+	_, err = db.SignIn(authData)
+	if err != nil {
+		panic(err)
 	}
 
 	return db, nil
 }
 
-func Select[T any](from string) (*[]T, error) {
-	res, err := SurrealDBConn.Query(fmt.Sprintf("SELECT * FROM %s", from), nil)
-	if err != nil {
-		return nil, err
-	}
+// func Select[T any](from string) (*[]T, error) {
+// 	res, err := SurrealDBConn.Select(fmt.Sprintf("SELECT * FROM %s", from), nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	var data []T
-	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
-		return nil, err
-	}
+// 	var data []T
+// 	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
+// 		return nil, err
+// 	}
 
-	return &data, nil
-}
+// 	return &data, nil
+// }
 
-func SelectOnly[T any](from string) (*T, error) {
-	res, err := SurrealDBConn.Query(fmt.Sprintf("SELECT * FROM %s", from), nil)
-	if err != nil {
-		return nil, err
-	}
+// func SelectOnly[T any](from string) (*T, error) {
+// 	res, err := SurrealDBConn.Query(fmt.Sprintf("SELECT * FROM %s", from), nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	var data []T
-	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
-		return nil, err
-	}
+// 	var data []T
+// 	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
+// 		return nil, err
+// 	}
 
-	if len(data) == 1 {
-		return &data[0], nil
-	}
+// 	if len(data) == 1 {
+// 		return &data[0], nil
+// 	}
 
-	return nil, fmt.Errorf("more than record returned")
-}
+// 	return nil, fmt.Errorf("more than record returned")
+// }
 
-func Graph[T any](from string) (*T, error) {
-	res, err := SurrealDBConn.Query(fmt.Sprintf("SELECT *,fn::recurse($this) AS children FROM %s;", from), map[string]interface{}{})
-	if err != nil {
-		return nil, err
-	}
+// func Graph[T any](from string) (*T, error) {
+// 	res, err := SurrealDBConn.Select(fmt.Sprintf("SELECT *,fn::recurse($this) AS children FROM %s;", from), map[string]interface{}{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	data := make([]T, 0)
-	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
-		return nil, err
-	}
+// 	data := make([]T, 0)
+// 	if ok, err := surrealdb.UnmarshalRaw(res, &data); !ok {
+// 		return nil, err
+// 	}
 
-	return &data[0], nil
-}
+// 	return &data[0], nil
+// }
